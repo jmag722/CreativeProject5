0546adc4a930781ef75de0ffd27806f9675430b2
Jared Magnusson <jmag722@gmail.com>
2018-11-12 23:06:16 +0000

    made basic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+
+
+router.post('/event', function(req, res, next) {
+    console.log("POST new event route");
+    res.sendStatus(200);
+});
+
+router.get('/event', function(req, res, next) {
+    console.log("GET event route");
 });
 
 module.exports = router;
